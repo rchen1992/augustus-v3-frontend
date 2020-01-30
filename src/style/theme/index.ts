2eaa1eb69dfd485572dd5b85716728ac5a76b45e
Raymond Chen <rchen1992@gmail.com>
2020-01-30 09:37:30 -0800

    Make spacing helper more useful.

--- src/style/theme/index.ts
+++ src/style/theme/index.ts
@@ -4,7 +4,7 @@ import colors, { Colors } from './colors';
 import typography, { Typography } from './typography';
 
 export interface BaseTheme {
-    spacing: (index: number) => string;
+    spacing: (index: number, hIndex: number) => string;
     spacingValues: (index: number) => number;
     colors: Colors;
     typography: Typography;
@@ -13,13 +13,20 @@ export interface BaseTheme {
 const theme: DefaultTheme = {
     /**
      * Helper to return spacing in pixels.
+     * If 1 number is provided, returns spacing for all directions.
+     * If 2 numbers are provided, the first index acts as
+     *  vertical spacing, and second index acts as horizontal spacing.
      */
-    spacing: (index: number) => {
-        return `${spacing[index]}px`;
+    spacing: (index: number, hIndex?: number) => {
+        let space = `${spacing[index]}px`;
+        if (hIndex) {
+            space += ` ${spacing[hIndex]}px`;
+        }
+        return space;
     },
 
     /**
-     * Helper to return spacing as a raw number.
+     * Helper to return spacing values as a raw number.
      */
     spacingValues: (index: number) => {
         return spacing[index];
