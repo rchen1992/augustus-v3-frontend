7777c5de38d12f22a612350837707e4d40826411
Raymond Chen <rchen1992@gmail.com>
2020-01-30 22:09:19 -0800

    Add gray color palette to theme.

--- src/style/theme/colors.ts
+++ src/style/theme/colors.ts
@@ -1,10 +1,26 @@
 export interface Colors {
     seaGreen: string;
     primary: string;
+    grayValues: string[];
+    gray: (index: number) => string;
 }
+
+export const grays = [
+    'hsl(204, 8%, 98%)',
+    'hsl(204, 8%, 95%)',
+    'hsl(204, 8%, 90%)',
+    'hsl(204, 8%, 86%)',
+    'hsl(204, 8%, 81%)',
+    'hsl(204, 8%, 76%)',
+    'hsl(204, 5%, 67%)',
+    'hsl(204, 4%, 58%)',
+    'hsl(204, 3%, 49%)',
+    'hsl(204, 3%, 40%)',
+];
 
 const colors = {
     seaGreen: 'hsl(161, 70%, 38.1%)',
+    grayValues: grays,
 };
 
 const namedColors = {
@@ -14,4 +30,6 @@ const namedColors = {
 export default {
     ...colors,
     ...namedColors,
+
+    gray: (index: number) => grays[index],
 };
