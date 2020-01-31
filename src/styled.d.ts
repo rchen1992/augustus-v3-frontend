c043dbe769d240fc43373ed54cb1409104e296a6
Raymond Chen <rchen1992@gmail.com>
2020-01-30 22:06:53 -0800

    Fix theme import.

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,6 +1,6 @@
 // import original module declarations
 import 'styled-components';
-import { Theme } from 'style/theme';
+import { BaseTheme } from 'style/theme';
 
 // and extend them!
 declare module 'styled-components' {
