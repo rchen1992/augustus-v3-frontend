1cca885ae7f17a93f8a112c54da7be5c1384dfd3
Raymond Chen <rchen1992@gmail.com>
2020-02-02 15:23:35 -0800

    Remove unused import.

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { PageHeader, Button, Icon } from 'antd';
+import { PageHeader, Button } from 'antd';
 
 interface SectionHeaderProps {
     title: string;
