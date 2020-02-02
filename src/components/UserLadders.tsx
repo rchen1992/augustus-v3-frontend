53a2ce87211ef5a405e352913d66a44ff544ff64
Raymond Chen <rchen1992@gmail.com>
2020-02-02 11:29:37 -0800

    Fix tslint error.

--- src/components/UserLadders.tsx
+++ src/components/UserLadders.tsx
@@ -4,34 +4,6 @@ import UserLadder from 'components/UserLadder';
 import styled from 'styled-components';
 import media from 'style/media';
 
-const Container = styled.div`
-    display: grid;
-    grid-gap: ${({ theme }) => theme.spacing(2)}};
-
-    ${media.sm`
-        grid-gap: ${({ theme }) => theme.spacing(4)}};
-    `};
-
-    ${media.smd`
-        grid-template-columns: 1fr 1fr;
-        grid-gap: ${({ theme }) => theme.spacing(2)}};
-    `};
-
-    ${media.mlg`
-        grid-gap: ${({ theme }) => theme.spacing(4)}};
-    `};
-
-    ${media.lg`
-        grid-template-columns: 1fr 1fr 1fr;
-        grid-gap: ${({ theme }) => theme.spacing(3)}};
-    `};
-
-    ${media.xl`
-        grid-template-columns: 1fr 1fr 1fr 1fr;
-        grid-gap: ${({ theme }) => theme.spacing(4)}};
-    `};
-`;
-
 const UserLadders: React.FC = () => {
     const { loading, data } = useGetUserLaddersQuery();
 
@@ -47,3 +19,31 @@ const UserLadders: React.FC = () => {
 };
 
 export default UserLadders;
+
+const Container = styled.div`
+    display: grid;
+    grid-gap: ${({ theme }) => theme.spacing(2)};
+
+    ${media.sm`
+        grid-gap: ${({ theme }) => theme.spacing(4)};
+    `}
+
+    ${media.smd`
+        grid-template-columns: 1fr 1fr;
+        grid-gap: ${({ theme }) => theme.spacing(2)};
+    `}
+
+    ${media.mlg`
+        grid-gap: ${({ theme }) => theme.spacing(4)};
+    `}
+
+    ${media.lg`
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-gap: ${({ theme }) => theme.spacing(3)};
+    `}
+
+    ${media.xl`
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+        grid-gap: ${({ theme }) => theme.spacing(4)};
+    `}
+`;
