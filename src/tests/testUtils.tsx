b64527b2f191e8f64de0b7b7ad4665c40a9dfe91
Raymond Chen <rchen1992@gmail.com>
2020-02-08 21:54:13 -0800

    Add Apollo MockedProvider to test utils.

--- src/tests/testUtils.tsx
+++ src/tests/testUtils.tsx
@@ -3,11 +3,14 @@ import { render } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
 import theme from 'style/theme';
 import { BrowserRouter } from 'react-router-dom';
+import { MockedProvider, MockedResponse } from '@apollo/react-testing';
 
-export function renderWithProvider(element: React.ReactElement) {
+export function renderWithProvider(element: React.ReactElement, mocks?: readonly MockedResponse[]) {
     return render(
-        <ThemeProvider theme={theme}>
-            <BrowserRouter>{element}</BrowserRouter>
-        </ThemeProvider>
+        <MockedProvider mocks={mocks} addTypename={false}>
+            <ThemeProvider theme={theme}>
+                <BrowserRouter>{element}</BrowserRouter>
+            </ThemeProvider>
+        </MockedProvider>
     );
 }
