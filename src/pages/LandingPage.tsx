8b5703441543fafe395956606f9eb33df799be86
Raymond Chen <rchen1992@gmail.com>
2020-03-10 21:43:38 -0700

    Responsive landing page.

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,6 +3,7 @@ import { useAuth0 } from 'providers/Auth0Provider';
 import styled from 'styled-components';
 import AugustusLogo from 'components/AugustusLogo';
 import { Button } from 'antd';
+import media from 'style/media';
 
 function LandingPage() {
     const { loginWithRedirect } = useAuth0();
@@ -45,27 +46,27 @@ function LandingPage() {
             <FeatureSection>
                 <MaxWidthContainer>
                     <FeatureContainer>
-                        <div>
+                        <FeatureCol>
                             <FeatureTitle>Create or join ladders</FeatureTitle>
                             <FeatureDescription>
                                 Create a new ladder or get invited to join an existing ladder by one
                                 of your friends. Track your rank, rating, wins, and losses in each
                                 of your ladders!
                             </FeatureDescription>
-                        </div>
+                        </FeatureCol>
                         <ColImage src="/userLadder.png" />
                     </FeatureContainer>
-                    <FeatureContainer>
+                    <FeatureContainerReverse>
                         <ColImage src="/userMatches.png" />
-                        <div>
+                        <FeatureCol>
                             <FeatureTitle>Log your matches</FeatureTitle>
                             <FeatureDescription>
                                 After you play a match, either you or your opponent can log the
                                 result, and your ratings will be instantly updated. Keep track of
                                 your full match history!
                             </FeatureDescription>
-                        </div>
-                    </FeatureContainer>
+                        </FeatureCol>
+                    </FeatureContainerReverse>
                 </MaxWidthContainer>
             </FeatureSection>
             <DarkSection>
@@ -94,7 +95,15 @@ const MaxWidthContainer = styled.div`
 `;
 
 const Hero = styled.div`
-    padding: ${({ theme }) => theme.spacing(3, 4, 10, 4)};
+    padding: ${({ theme }) => theme.spacing(3, 4, 8, 4)};
+
+    ${media.sm`
+        padding: ${({ theme }) => theme.spacing(3, 4, 9, 4)};
+    `}
+
+    ${media.md`
+        padding: ${({ theme }) => theme.spacing(3, 4, 10, 4)};
+    `}
 `;
 
 const Header = styled.header`
@@ -116,17 +125,38 @@ const TitleContainer = styled.div`
 `;
 
 const Title = styled.h1`
-    font-size: 60px;
+    font-size: 30px;
     font-weight: bold;
     margin-bottom: 0;
+
+    ${media.sm`
+        font-size: 40px;
+    `}
+
+    ${media.md`
+        font-size: 60px;
+    `}
 `;
 
 const TagLine = styled.p`
-    font-size: 20px;
+    font-size: 16px;
+
+    ${media.sm`
+        font-size: 18px;
+    `}
+
+    ${media.md`
+        font-size: 20px;
+    `}
 `;
 
 const DarkSection = styled.section`
     background-color: hsl(200, 20%, 25%);
+    padding: ${({ theme }) => theme.spacing(2, 2)};
+
+    ${media.md`
+        padding: ${({ theme }) => theme.spacing(null, 2)};
+    `}
 `;
 
 const LadderRankingsContainer = styled.div`
@@ -135,7 +165,15 @@ const LadderRankingsContainer = styled.div`
     align-items: center;
     flex-direction: column;
     position: relative;
-    top: -160px;
+    top: -60px;
+
+    ${media.sm`
+        top: -100px;
+    `}
+
+    ${media.md`
+        top: -160px;
+    `}
 
     img {
         width: 80%;
@@ -145,8 +183,16 @@ const LadderRankingsContainer = styled.div`
 const LadderRankingsDescription = styled.div`
     color: white;
     position: relative;
-    top: 80px;
+    top: 40px;
     text-align: center;
+
+    ${media.sm`
+        top: 50px;
+    `}
+
+    ${media.md`
+        top: 80px;
+    `}
 
     h2 {
         color: white;
@@ -154,29 +200,68 @@ const LadderRankingsDescription = styled.div`
 `;
 
 const FeatureSection = styled.section`
-    padding: ${({ theme }) => theme.spacing(5)};
+    padding: ${({ theme }) => theme.spacing(2, 3)};
+
+    ${media.md`
+        padding: ${({ theme }) => theme.spacing(5)};
+    `}
+`;
+
+const FeatureCol = styled.div`
+    text-align: center;
+
+    ${media.md`
+        text-align: left;
+        padding: ${({ theme }) => theme.spacing(null, 4)};
+    `}
 `;
 
 const FeatureTitle = styled.h2`
-    font-size: 30px;
+    font-size: 20px;
     font-weight: bold;
+
+    ${media.md`
+        font-size: 30px;
+    `}
 `;
 
 const FeatureDescription = styled.p`
-    font-size: 18px;
+    font-size: 14px;
     line-height: 28px;
     max-width: 500px;
+
+    ${media.md`
+        font-size: 18px;
+    `}
 `;
 
 const FeatureContainer = styled.div`
     display: flex;
+    flex-direction: column;
     justify-content: space-around;
     align-items: center;
-    padding: ${({ theme }) => theme.spacing(6, 0)};
+    padding: ${({ theme }) => theme.spacing(3, 0)};
+
+    ${media.md`
+        flex-direction: row;
+        padding: ${({ theme }) => theme.spacing(6, 0)};
+    `}
+`;
+
+const FeatureContainerReverse = styled(FeatureContainer)`
+    flex-direction: column-reverse;
 `;
 
 const ColImage = styled.img`
-    width: 40%;
+    width: 100%;
+
+    ${media.sm`
+        width: 80%;
+    `}
+
+    ${media.md`
+        width: 40%;
+    `}
 `;
 
 const Footer = styled.footer`
@@ -184,7 +269,11 @@ const Footer = styled.footer`
     align-items: center;
     flex-direction: column;
     text-align: center;
-    padding: ${({ theme }) => theme.spacing(5)};
+    padding: ${({ theme }) => theme.spacing(3)};
+
+    ${media.md`
+        padding: ${({ theme }) => theme.spacing(5)};
+    `}
 
     h2,
     p {
