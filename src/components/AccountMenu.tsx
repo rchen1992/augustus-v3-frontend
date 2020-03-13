7a686d4581b744cfb98b4498a2d31f9a64400afa
Raymond Chen <rchen1992@gmail.com>
2020-03-12 21:51:29 -0700

    Add returnTo url to fix logout.

--- src/components/AccountMenu.tsx
+++ src/components/AccountMenu.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Menu, Dropdown } from 'antd';
 import { useAuth0 } from 'providers/Auth0Provider';
 import AccountModal from 'components/AccountModal';
+import authConfig from 'config/auth_config';
 
 const AccountMenu: React.FC = ({ children }) => {
     const { logout } = useAuth0();
@@ -12,7 +13,16 @@ const AccountMenu: React.FC = ({ children }) => {
             <Menu.Item>
                 <span onClick={() => setAccountModalVisible(true)}>My Account</span>
             </Menu.Item>
-            <Menu.Item onClick={() => logout()}>Log out</Menu.Item>
+            <Menu.Item
+                onClick={() =>
+                    logout({
+                        returnTo: process.env.REACT_APP_FRONTEND_DOMAIN,
+                        client_id: authConfig.clientId,
+                    })
+                }
+            >
+                Log out
+            </Menu.Item>
         </Menu>
     );
 
