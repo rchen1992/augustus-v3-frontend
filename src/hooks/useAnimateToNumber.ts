4716bad2a7515fd3f7a0adba18481091794b4d1c
Raymond Chen <rchen1992@gmail.com>
2020-03-15 17:19:59 -0700

    Update useAnimateToNumber hook to handle animating in either direction. Also add support for changing animation behavior after the number has initially rendered.

--- src/hooks/useAnimateToNumber.ts
+++ src/hooks/useAnimateToNumber.ts
@@ -1,47 +1,94 @@
 import React, { useEffect, useRef } from 'react';
 
 interface Options {
+    /**
+     * The number to animate to.
+     */
     targetNumber: number;
+
+    /**
+     * The number to start at.
+     */
     startNumber?: number;
+
+    /**
+     * The rate of change of the number for every tick of the animation.
+     */
     changePerTick?: number;
+
+    /**
+     * The rate of change of the number for every tick of the animation,
+     * after the animation has already run once.
+     * This can be used to differentiate between the initial animation and
+     * any follow up animations that may change the current state of the number.
+     */
+    changePerTickAfterInitialRun?: number;
+
+    /**
+     * How many milliseconds elapse in between each tick of the animation.
+     */
     tickSpeed?: number;
+
+    /**
+     * The React ref of the DOM element to operate on.
+     * The animation will change the innerHTML of this element.
+     */
     element: React.MutableRefObject<any>;
 }
 
 /**
  * Animates a number to its target.
- *
- * Currently only handles animating upwards,
- * but should be easily adjustable to count downwards.
  */
 export default ({
     startNumber = 0,
     targetNumber,
     element,
     changePerTick = 10,
+    changePerTickAfterInitialRun = 1,
     tickSpeed = 10,
 }: Options) => {
     const currentNumber = useRef(startNumber);
 
     useEffect(() => {
-        currentNumber.current = startNumber;
+        /**
+         * Are we animating to a higher number of lower number?
+         */
+        const goingUp = targetNumber - currentNumber.current > 0;
+
+        /**
+         * Has this animation run at least once?
+         */
+        const finishedInitialRun = currentNumber.current !== startNumber;
 
         const interval = setInterval(() => {
-            if (currentNumber.current >= targetNumber) {
+            if (
+                currentNumber.current === targetNumber ||
+                (goingUp && currentNumber.current > targetNumber) ||
+                (!goingUp && currentNumber.current < targetNumber)
+            ) {
                 clearInterval(interval);
                 return;
             }
 
             /**
+             * Determine how much we are changing, regardless of direction.
+             *
              * If our rate of change is larger than the difference between the
-             * current number and the target number, we lower the rate of change to 1
+             * current number and the target number, we forcefully lower the magnitude to 1
              * to make sure we hit the target number exactly.
+             *
+             * If we've already run the animation once, we should use `changePerTickAfterInitialRun`.
              */
-            if (targetNumber - currentNumber.current < changePerTick) {
-                currentNumber.current += 1;
-            } else {
-                currentNumber.current += changePerTick;
+            let changeMagnitude = changePerTick;
+            if (Math.abs(targetNumber - currentNumber.current) < changePerTick) {
+                changeMagnitude = 1;
+            } else if (finishedInitialRun) {
+                changeMagnitude = changePerTickAfterInitialRun;
             }
+
+            const changeDirection = goingUp ? 1 : -1;
+
+            currentNumber.current += changeMagnitude * changeDirection;
 
             requestAnimationFrame(() => {
                 element.current.innerHTML = currentNumber.current;
