53be987b99196ac87e4a19b6290fd57de1884da0
Raymond Chen <rchen1992@gmail.com>
2020-03-16 21:41:06 -0700

    Hide avatars in ladder rankings and ladder matches tables on mobile to free up space.

--- src/components/AvatarAndUsername.tsx
+++ src/components/AvatarAndUsername.tsx
@@ -1,23 +1,47 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Avatar } from 'antd';
+import media from 'style/media';
 
 interface AvatarAndUsernameProps {
     avatarUrl?: string | null;
     userName: string;
+    hideAvatarOnMobile?: boolean;
 }
 
-const AvatarAndUsername: React.FC<AvatarAndUsernameProps> = ({ avatarUrl, userName }) => {
+interface WrapperProps {
+    hideAvatarOnMobile?: boolean;
+}
+
+const AvatarAndUsername: React.FC<AvatarAndUsernameProps> = ({
+    avatarUrl,
+    userName,
+    hideAvatarOnMobile = true,
+}) => {
     return (
         <span>
-            <Avatar src={avatarUrl || undefined} />
-            <UsernameWrapper>{userName}</UsernameWrapper>
+            <AvatarWrapper hideAvatarOnMobile={hideAvatarOnMobile}>
+                <Avatar src={avatarUrl || undefined} />
+            </AvatarWrapper>
+            <UsernameWrapper hideAvatarOnMobile={hideAvatarOnMobile}>{userName}</UsernameWrapper>
         </span>
     );
 };
 
 export default AvatarAndUsername;
 
-const UsernameWrapper = styled.span`
-    margin-left: ${({ theme }) => theme.spacing(1)};
+const AvatarWrapper = styled.span<WrapperProps>`
+    display: ${props => (props.hideAvatarOnMobile ? 'none' : 'inline-block')};
+
+    ${media.sm`
+        display: inline-block;
+    `}
+`;
+
+const UsernameWrapper = styled.span<WrapperProps>`
+    margin-left: ${props => (props.hideAvatarOnMobile ? '0' : props.theme.spacing(1))};
+
+    ${media.sm`
+        margin-left: ${({ theme }) => theme.spacing(1)};
+    `}
 `;
